59235f365e1c730f6c37516c42f144ace62dff09
ethender <ethender.9966526462@gmail.com>
2017-10-29 08:40:08 +0530

    sql table

---
+++ sql/SWIFTDB.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS swifttrade;
+
+USE swifttrade;
+
+CREATE TABLE trading(
+tradeid INT PRIMARY KEY AUTO_INCREMENT,
+tradedate VARCHAR(100),
+tradestate varchar(75),
+stocks varchar(250)
+);
+
+
+CREATE TABLE stock(
+stockid INT PRIMARY KEY AUTO_INCREMENT,
+symbol VARCHAR(150) UNIQUE NOT NULL,
+series VARCHAR(25),
+openprice FLOAT(15),
+highprice FLOAT(15),
+lowprice FLOAT(15),
+ltp FLOAT(15),
+previousprice FLOAT(15),
+netprice FLOAT(15),
+tradedquantity FLOAT(15),
+turnoverinlakhs FLOAT(15),
+lastcropannouncementdate VARCHAR(75),
+lastcropannouncement VARCHAR(255)
+);
+
