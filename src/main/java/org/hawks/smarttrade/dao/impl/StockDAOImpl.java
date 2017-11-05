b259cc62e9389a4831a3ba3977f7f80266bb065b
ethender <ethender.9966526462@gmail.com>
2017-11-05 20:09:33 +0530

    stock DAO

--- src/main/java/org/hawks/smarttrade/dao/impl/StockDAOImpl.java
+++ src/main/java/org/hawks/smarttrade/dao/impl/StockDAOImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.hawks.smarttrade.dao.StockDAO;
 import org.hawks.smarttrade.model.StockDB;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 
@@ -38,38 +39,69 @@ public class StockDAOImpl extends JdbcDaoSupport  implements StockDAO{
 
 	@Override
 	public boolean updateStock(StockDB stock) {
-		// TODO Auto-generated method stub
-		return false;
+		String sql = "UPDATE stock WHERE "
+				+ "symbol=?,series=?,openprice=?,"
+				+ "highprice=?,lowprice=?,ltp=?,"
+				+ "previousprice=?,netprice=?,"
+				+ "tradedquantity=?,lastcropannouncementdate=?,"
+				+ "lastcropannouncement=?,stockstate=? WHERE stockid=?";
+		Object[] data = new Object[] {
+				stock.getSymbol(),
+				stock.getSeries(),
+				stock.getOpenprice(),
+				stock.getHighprice(),
+				stock.getLowprice(),
+				stock.getLtp(),
+				stock.getPreviousprice(),
+				stock.getNetprice(),
+				stock.getTradedquantity(),
+				stock.getTurnoverinlakhs(),
+				stock.getLastcropannouncementdate(),
+				stock.getLastcropannouncement(),
+				stock.getStockstate(),
+				stock.getStockid()
+		};
+		
+		getJdbcTemplate().update(sql, data);
+		return true;
 	}
 
 	@Override
 	public StockDB findStockById(StockDB stock) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM stock WHERE stockid=?";
+		
+		List<StockDB> stocks = getJdbcTemplate().query(sql, new Object[] {stock.getStockid()}, new BeanPropertyRowMapper(StockDB.class));
+		return stocks.get(0);
 	}
 
 	@Override
 	public List<StockDB> findStockByState(StockDB stock) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM stock WHERE stockstate=?";
+		
+		List<StockDB> stocks = getJdbcTemplate().query(sql, new Object[] {stock.getStockstate()}, new BeanPropertyRowMapper(StockDB.class));
+		return stocks;
 	}
 
 	@Override
 	public List<StockDB> getAllStocks() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM stock";
+		
+		List<StockDB> stocks = getJdbcTemplate().query(sql, new BeanPropertyRowMapper(StockDB.class));
+		return stocks;
 	}
 
 	@Override
 	public boolean deleteStockById(StockDB stock) {
-		// TODO Auto-generated method stub
-		return false;
+		String sql  = "DELETE FROM stock WHERE stockid=?";
+		getJdbcTemplate().update(sql, new Object[] {stock.getStockid()});
+		return true;
 	}
 
 	@Override
 	public boolean deleteStockByState(StockDB stock) {
-		// TODO Auto-generated method stub
-		return false;
+		String sql = "DELETE FROM stock WHERE stockstate=?";
+		getJdbcTemplate().update(sql, new Object[] {stock.getStockstate()});
+		return true;
 	}
 
 }
